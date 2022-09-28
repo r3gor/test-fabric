5724e74867950b5a8f67b9f58b1b90b559050e8e
Roger Ramos <rogrp6@gmail.com>
2022-09-28 07:10:52 -0500

    fix: contact path

--- pharma-ledger-network/up-fabric.sh
+++ pharma-ledger-network/up-fabric.sh
@@ -4,8 +4,8 @@ sudo ./net-pln.sh createChannel
 
 # install npm dependencies
 mydir=`pwd`
-cd ~/PLN-dapp/pharma-ledger-network/organizations/manufacturer/contract
+cd ./organizations/manufacturer/contract
 npm i
-cd mydir
+cd $mydir
 
 sudo ./net-pln.sh deploySmartContract
